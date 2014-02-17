0ffd8d8c6b82873e3a6fe7738ad2c5b826dbe839
Quinn Liu <quinnliu@vt.edu>
2014-02-17 10:18:41 -0500

    added more explanation and formatted code

---
+++ gaussianDistributionOfNeuronResponseRate/README.md
@@ -0,0 +1,8 @@
+Ploting Neuron Response Rates
+=============================
+
+The task here is to find the optimal threshold between 2 neuron responses by 
+graphing 2 different neuron responses and recording the point at which the 2 graphs (guassian distributions)
+intersect. 
+
+To understand more watch this [video](http://www.doingisunderstanding.com/Videos/Neuronal_Decoding)!
--- gaussianDistributionOfNeuronResponseRate/runExample.m
+++ gaussianDistributionOfNeuronResponseRate/runExample.m
@@ -1,9 +1,12 @@
-%you need the Symbolic Math toolbox as well as MATLAB for this script.
-p=randn(60,1); %col. vector of 60 random numbers from N(0,1) is your random sample
-%arithmetic mean mu = mean(p); 
-%standard deviation  sigma = std(p); 
+p=randn(60,1); % column vector of 60 random numbers from N(0,1) is your random sample
+
+% arithmetic mean mu = mean(p); 
+% standard deviation  sigma = std(p); 
+
 mu5 = 5; sigma5 = .5; % produces S1 normal distribution
+
 mu7 = 7; sigma1 = 1; % produces S2 normal distribution
+
 if sigma5^2 ~= 0 % standard deviation squared must be non-zero 
 %probability density function of normal distribution curve
 %i.e. Gaussian function to use with ezplot
@@ -11,7 +14,7 @@ if sigma5^2 ~= 0 % standard deviation squared must be non-zero
     pdf7 = @(x) exp( -(((x-mu7).^2.)/(2*sigma1^2)) )/((sqrt(2*pi))*sigma1);   
    
     %plot the normal probability curve
-     fg = figure;
+    fg = figure;
   
       pdf5data = pdf5(p);
       pdf7data = pdf7(p);
